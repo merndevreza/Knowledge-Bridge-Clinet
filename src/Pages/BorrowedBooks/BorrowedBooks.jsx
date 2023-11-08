e7c12b99140b0d51a6d36872ea081bc2cbf9bbf6
merndevreza <merndevreza@gmail.com>
2023-11-08 21:51:02 +0600

    Delete borrowed books successfully

--- src/Pages/BorrowedBooks/BorrowedBooks.jsx
+++ src/Pages/BorrowedBooks/BorrowedBooks.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
 import axios from "axios";
-import BorrowedBooksCard from "./BorrowedBooksCard";
+import Swal from "sweetalert2";
 
 const BorrowedBooks = () => {
   const { currentUser } = useContext(AuthContext);
@@ -18,15 +18,78 @@ const BorrowedBooks = () => {
       });
   }, [currentUser]);
 
-  return (
+  const handleReturnNow = (id) => {
+    axios
+      .delete(`http://localhost:5000/borrowed-books/${id}`)
+      .then((res) => {
+        if (res.data.deletedCount > 0) {
+          Swal.fire({
+            title: "Congrats!!",
+            text: "You Deleted successfully.",
+            icon: "success",
+            confirmButtonText: "OK",
+          });
+          axios
+          .get(`http://localhost:5000/borrowed-books/${currentUser.email}`)
+          .then((res) => {
+            setLoadedBorrowedBooks(res.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        }
+      })
+      .catch((error) => {
+        Swal.fire({
+          title: "Error!",
+          text: `${error.message}`,
+          icon: "error",
+          confirmButtonText: "OK",
+        });
+      });
+  };
 
-   <div className="dark:bg-theme-dark bg-white px-2 py-[100px]">
-       <div className="max-w-3xl mx-auto flex flex-col gap-5">
-      {loadedBorrowedBooks.map((item) => (
-        <BorrowedBooksCard key={item._id} item={item}></BorrowedBooksCard>
-      ))}
+  return (
+    <div className="dark:bg-theme-dark bg-white px-2 py-[100px]">
+      <div className="max-w-3xl mx-auto flex flex-col gap-5">
+        {loadedBorrowedBooks.map((item) => (
+          <div
+            key={item._id}
+            className="flex p-4 border-2 gap-4 dark:border-[#c0c0c0]"
+          >
+            <div>
+              <img className="w-[200px]" src={item.book.photo} alt="" />
+            </div>
+            <div className="flex flex-col justify-center w-full">
+              <div>
+                <h2 className="text-xl md:text-3xl text-theme-golden">
+                  {item.book.bookName}
+                </h2>
+                <p className="capitalize font-bold text-xl dark:text-white">
+                  Category: {item.book.category}
+                </p>
+              </div>
+              <div className="mt-5">
+                <p className="text-lg font-bold dark:text-white">
+                  Borrowed Date: {item.borrowedDate}
+                </p>
+                <p className="text-lg font-bold text-theme-golden">
+                  Return Date: {item.returnDate}
+                </p>
+              </div>
+              <div className="mt-5 text-end">
+                <button
+                  onClick={() => handleReturnNow(item._id)}
+                  className="btn rounded-full bg-theme-golden border-none hover:bg-theme-black dark:hover:bg-white dark:hover:text-theme-golden text-lg font-bold text-white px-6"
+                >
+                  Return Now
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-   </div>
   );
 };
 
--- src/Pages/BorrowedBooks/BorrowedBooksCard.jsx
+++ src/Pages/BorrowedBooks/BorrowedBooksCard.jsx
@@ -3,29 +3,7 @@ const BorrowedBooksCard = ({ item }) => {
   const { bookName, photo, category } = book;
 
   return (
-    <div className="flex p-4 border-2 gap-4 dark:border-[#c0c0c0]">
-      <div>
-        <img className="w-[200px]" src={photo} alt="" />
-      </div>
-      <div className="flex flex-col justify-center w-full">
-        <div>
-          <h2 className="text-xl md:text-3xl text-theme-golden">{bookName}</h2>
-          <p className="capitalize font-bold text-xl dark:text-white">Category: {category}</p>
-        </div>
-        <div className="mt-5">
-          <p className="text-lg font-bold dark:text-white">Borrowed Date: {borrowedDate}</p>
-          <p className="text-lg font-bold text-theme-golden">
-            Return Date: {returnDate}
-          </p>
-        </div>
-        <div className="mt-5 text-end">
-        <button className="btn rounded-full bg-theme-golden border-none hover:bg-theme-black dark:hover:bg-white dark:hover:text-theme-golden text-lg font-bold text-white px-6">
-          Return Now
-        </button>
-      </div>
-      </div>
-      
-    </div>
+   <div></div>
   );
 };
 
