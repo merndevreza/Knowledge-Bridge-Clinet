4eddf7aa5fe36baaa982e31194dfac9424f8bda6
merndevreza <merndevreza@gmail.com>
2023-11-08 23:54:28 +0600

    footer and banner added

--- src/Pages/Home/Sections/Hero.jsx
+++ src/Pages/Home/Sections/Hero.jsx
@@ -1,8 +1,32 @@
-import banner from "../../../assets/images/banner.jpg"
+import banner1 from "../../../assets/images/banner-6.jpg"
+import banner2 from "../../../assets/images/banner-5.jpg"
+import banner3 from "../../../assets/images/banner-2.jpg"
+import banner4 from "../../../assets/images/banner-3.jpg"
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 const Hero = () => {
    return (
       <div>
-         <img className="h-[700px] w-full object-cover object-bottom" src={banner} alt="" />
+         <Swiper
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper md:h-[900px] "
+      >
+        <SwiperSlide >
+            <img className="w-full  h-full object-cover object-center" src={banner1} alt="" />
+          </SwiperSlide>
+        <SwiperSlide >
+            <img className="w-full  h-full object-cover object-center" src={banner2} alt="" />
+          </SwiperSlide>
+        <SwiperSlide >
+            <img className="w-full  h-full object-cover object-center" src={banner3} alt="" />
+          </SwiperSlide>
+        <SwiperSlide >
+            <img className="w-full  h-full object-cover object-center" src={banner4} alt="" />
+          </SwiperSlide>
+      </Swiper>
       </div>
    );
 };
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,39 @@
-
+import { Link } from "react-router-dom";
+import { RiTwitterXFill } from "react-icons/ri";
+import { FaLinkedinIn, FaFacebookF, FaInstagram } from "react-icons/fa";
+import footerLogo from "../../../assets/images/logo/logo-2.png";
 const Footer = () => {
-   return (
-      <div>
-         FOOTER
+  return (
+    <footer className="bg-theme-black border-t-4 border-theme-orange-red">
+      <div className=" footer footer-center p-10 bg-[#03112c6c] text-[#fff]">
+        <aside>
+          <img src={footerLogo} alt="" />
+          <p className="text-lg mt-5 lg:text-xl">
+            Connect Your Knowledge with Genius  
+          </p>
+        </aside>
+        <nav>
+          <div className="grid grid-flow-col gap-4">
+            <Link to="/">
+              <FaLinkedinIn></FaLinkedinIn>
+            </Link>
+            <Link to="/">
+              <RiTwitterXFill></RiTwitterXFill>
+            </Link>
+            <Link to="/">
+              <FaFacebookF></FaFacebookF>
+            </Link>
+            <Link to="/">
+              <FaInstagram></FaInstagram>
+            </Link>
+          </div>
+        </nav>
       </div>
-   );
+      <p className="bg-[#010114] text-center text-[#fff] py-4">
+        Copyright &copy; 2023, All right reserved by Rezaul Karim.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
